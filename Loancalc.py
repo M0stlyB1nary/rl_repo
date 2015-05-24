254ef1e3601146614ed6c20ff5004753ea6e66fd
M0stlyB1nary <idigchili@gmail.com>
2015-05-24 17:23:23 -0500

    Added Loancalc

---
+++ Loancalc.py
@@ -0,0 +1,9 @@
+def myloan():
+    prin = float(input('Loan Amount: '))
+    intrate = float(input('Interest %: '))/100
+    periodsYear = 12
+    years = float(input('Years: '))
+    mthly = (prin*intrate)/(periodsYear*(1-(1+(intrate/periodsYear))**(-1*years*periodsYear)))
+    mthly = round(mthly,2)
+    print('$', mthly)
+myloan()
